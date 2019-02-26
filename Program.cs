585d268140b08beb5421ac2f3502920ebda823af
azemobaida <47791218+azemobaida@users.noreply.github.com>
2019-02-26 18:54:22 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,15 +10,14 @@ namespace _26._02._19
 	{
 		static void Main(string[] args)
 		{
+			int counter = 1;
 			Console.WriteLine("please enter a number similar to the random number");
 			int a = Convert.ToInt32(Console.ReadLine());
-
 			Random engine = new Random();
-
 			int secretNumber = engine.Next(100) + 1;
-
 			while (a != secretNumber)
 			{
+				counter++;
 				if (a > secretNumber)
 				{
 					Console.WriteLine($"{a} is bigger");
@@ -29,11 +28,8 @@ namespace _26._02._19
 				}
 				a = Convert.ToInt32(Console.ReadLine());
 			}
-
-			
 			Console.WriteLine($"{a} is the right number ");
-			
-
+			Console.WriteLine($"{counter} times took you to guess the right number");
 		}
 	}
 }
